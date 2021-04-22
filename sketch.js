a4120fd059a37c3729d7d20fefeb92690b4cfbeb
jgardner78 <72280005+jgardner78@users.noreply.github.com>
2021-04-22 16:41:05 -0700

    1st commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function myFunc() {
+  if(cnt%2==0){document.getElementById("demo").innerHTML = "Hello World";}
+  else{document.getElementById("demo").innerHTML = "Goodbye World";}
+  cnt++;
+}
+
+function start(){
+	for(let j=0;j<10;j++){cell[j]=[];
+	for(let k=0;k<3;k++){cell[j][k]=0;
+	}}
+
+
+document.getElementById("demo").innerHTML = "Restart the Game "+cell[3][2];
+}
